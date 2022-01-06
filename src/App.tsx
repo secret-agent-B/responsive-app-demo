7bc2e2ed0f48bc2b75d17082c30ec8d2aa829806
Ryan Ad <raime910@msn.com>
2022-01-06 04:35:16 -0600

    Add App.tsx.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * The App component
+ */
+export default class App {
+  /**
+   * The render method.
+   * @return {JSX.Element}
+   */
+  render() {
+    return <div></div>;
+  }
+}
