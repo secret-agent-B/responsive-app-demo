6d95e3f31e1959e0c4f761d3896780805fa6c40c
Ryan Ad <raime910@msn.com>
2022-01-06 04:38:24 -0600

    Good code check.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { Component } from "react";
 
 /**
  * The App component
  */
-export default class App {
+export default class App extends Component {
   /**
    * The render method.
    * @return {JSX.Element}
    */
   render() {
-    return <div></div>;
+    return <div>	
+			
+This is a test.
+
+</div>;
   }
 }
