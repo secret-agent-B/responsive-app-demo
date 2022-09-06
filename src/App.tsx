22f715c7bbaa3bd51c42c5703bee980b311ec963
Ryan Adriano <raime910@msn.com>
2022-09-06 10:27:11 -0500

    WIP useResponsive hook.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import * as React from 'react'
+import {useResponsive} from './hooks/useResponsive';
 
 const App = () => {
-  return <div>Hello World!</div>
+  React.useEffect(() => {
+    console.log('hi')
+  })
+  const userBrowser = useResponsive('(min-width: 768px)')
+  return <>
+    <h1>Hello World!</h1>
+    <p>{`cssMediaQuery: ${userBrowser.cssMediaQuery}`}</p>
+    <p>{`The view port is ${userBrowser ? 'at least' : 'less than'} 768 pixels wide`}</p>
+  </>
 }
 
 export default App
